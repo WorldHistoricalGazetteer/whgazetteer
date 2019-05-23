00fdedc5f79faf1481c056b8aafa2cea76abd973
Karl Grossner <karl.geog@gmail.com>
2019-05-23 17:24:23 -0600

    external links; no modal iframes

--- whg/static/js/aliases.js
+++ whg/static/js/aliases.js
@@ -1,10 +1,12 @@
-base_urls= {
+base_urls = {
     "tgn": "http://vocab.getty.edu/page/tgn/",
     "dbp": "http://dbpedia.org/resource/",
     "gn": "http://www.geonames.org/",
     "wd": "https://www.wikidata.org/wiki/",
+    "loc": "http://id.loc.gov/authorities/names/",
+    "viaf": "http://viaf.org/viaf/",
     "wwf": "https://www.worldwildlife.org/ecoregions/",
     "whg": "/api/places/",
     "dplace": "http://dplace.org/societies/",
-    "indias": ""
+    "indias": "https://www.hgis-indias.net/dokuwiki/doku.php?id=gazetteer:"
 }
