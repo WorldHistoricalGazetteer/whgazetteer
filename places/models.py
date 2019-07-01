de8096334d455f102476f24cf90428b9b5d70638
Karl Grossner <karl.geog@gmail.com>
2019-07-01 15:36:05 +0200

    bits re: data munging

--- places/models.py
+++ places/models.py
@@ -19,6 +19,8 @@ class Place(models.Model):
     dataset = models.ForeignKey('datasets.Dataset', db_column='dataset',
         to_field='label', related_name='places', on_delete=models.CASCADE)
     ccodes = ArrayField(models.CharField(max_length=2))
+    # general purpose
+    flag = models.BooleanField(default=False)
 
     def __str__(self):
         # return str(self.id)
