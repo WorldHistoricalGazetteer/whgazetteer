e90a73a5c3cdab859feff0be41f936e509e595f6
Karl Grossner <karl.geog@gmail.com>
2019-10-03 21:08:55 +0100

    filtering suggest for highlighted

--- search/urls.py
+++ search/urls.py
@@ -3,12 +3,12 @@ from django.urls import path, include
 from django.conf.urls import url
 
 from search.views import (
-    advanced, SearchView, SuggestView,
+    advanced, SearchView, 
     fetchArea, FeatureContextView, TraceGeomView, UpdateCountsView)
 
 urlpatterns = [
     url(r'^search?$', SearchView.as_view(), name='search'),
-    url(r'^suggest?$', SuggestView.as_view(), name='suggest'),
+    #url(r'^suggest?$', SuggestView.as_view(), name='suggest'),
     url(r'^features?$', FeatureContextView.as_view(), name='feature_context'),
     url(r'^tracegeom?$', TraceGeomView.as_view(), name='trace_geom'),
     url(r'^updatecounts?$', UpdateCountsView.as_view(), name='update_counts'),
