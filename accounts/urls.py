d9102b84730551dc26f9b30ca325fff0d2c7c437
Karl Grossner <karl.geog@gmail.com>
2019-12-09 21:46:21 +0100

    conversion of profile page to form under way; needs extended fields

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('profile/', views.profile, name='profile'),
+    #path('<int:id>/detail', views.UserProfileView.as_view(), name='user-profile'),
+    path('profile/', views.UserProfileView.as_view(), name='user-profile'),
+    #path('profile/', views.profile, name='profile'),
 ]
