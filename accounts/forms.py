f3c7b355aaf45f2c9efc3848d6c114faa084e898
Karl Grossner <karl.geog@gmail.com>
2019-12-10 11:24:47 +0100

    user+profile form

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,15 +2,22 @@ from django import forms
 from django.contrib.auth.models import User, Group
 from django.db import models
 
+from accounts.models import Profile
 
-class UserProfileModelForm(forms.ModelForm):
+class UserModelForm(forms.ModelForm):
     
     class Meta:
         model = User
-        fields = ('username','email','password','first_name','last_name',
-                  #'affiliation','user_type','name'
-                )
-        #widgets = {
-        #'description': forms.Textarea(attrs={
-          #'rows':1,'cols': 40,'class':'textarea','placeholder':'brief description'}),
-        #}
+        fields = ('username','email','first_name','last_name',)
+        exclude = ('password',)
+
+class ProfileModelForm(forms.ModelForm):
+    
+    class Meta:
+        model = Profile
+        fields = ('name','affiliation','web_page','user_type')
+        widgets = {
+            'affiliation': forms.TextInput(attrs={'size': 60}),
+            'web_page': forms.TextInput(attrs={'size': 60}),
+            'password': forms.PasswordInput(attrs={'size': 60}),
+        }
--- accounts/views.py
+++ accounts/views.py
@@ -1,47 +1,40 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, Group
-from django.contrib import auth
+from django.contrib import auth, messages
+from django.db import transaction
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import UpdateView
 
-from accounts.forms import UserProfileModelForm
+from accounts.forms import UserModelForm, ProfileModelForm
 
-#def profile(request):
-class UserProfileView(UpdateView):
-    form_class = UserProfileModelForm
-    template_name = 'accounts/profile.html'
-
-    #print('request from profile',request.META.keys())
-    
-    def get_success_url(self):
-        id_ = self.kwargs.get("id")
-        return '/accounts/profile'
-    
-    def form_valid(self, form):
-        context={}
-        if form.is_valid():
-            print('form is valid')
-            print('cleaned_data: before ->', form.cleaned_data)
+@login_required
+@transaction.atomic
+def update_profile(request):
+    print('request.method',request.method)
+    context = {}
+    if request.method == 'POST':
+        user_form = UserModelForm(request.POST, instance=request.user)
+        profile_form = ProfileModelForm(request.POST, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, ('Your profile was successfully updated!'))
+            return redirect('profile')
         else:
-            print('form not valid', form.errors)
-            context['errors'] = form.errors
-        return super().form_valid(form)
-    
-    def get_object(self):
-        me = self.request.user
-        #print('args, kwargs:',self.args, self.kwargs)
-        id_ = me.id
-        return get_object_or_404(User, id=id_)
-    
-    def get_context_data(self, *args, **kwargs):
-        id_ = self.request.user.id
+            print('error, cleaned_data',user_form,profile_form)
+            messages.error(request, ('Please correct the error below.'))
+    else:
+        user_form = UserModelForm(instance=request.user)
+        profile_form = ProfileModelForm(instance=request.user.profile)
+        id_ = request.user.id
         u = get_object_or_404(User, id=id_)
-        context = super(UserProfileView, self).get_context_data(*args, **kwargs)
         context['groups'] = u.groups.values_list('name',flat=True)
-        return context
         
+    return render(request, 'accounts/profile.html', {
+        'user_form': user_form,
+        'profile_form': profile_form,
+        'context': context
+    })
     
-    #return render(request, 'accounts/profile.html', context=context)
-
 def register(request):
     if request.method == 'POST':
         if request.POST['password1'] == request.POST['password2']:
@@ -82,3 +75,43 @@ def logout(request):
     if request.method == 'POST':
         auth.logout(request)
         return redirect('home')
+
+    #class UserProfileView(UpdateView):
+        #form_class = (UserModelForm,ProfileModelForm)
+        #template_name = 'accounts/profile.html'
+    
+        ##print('request from profile',request.META.keys())
+        
+        #def get_success_url(self):
+            #id_ = self.kwargs.get("id")
+            #return '/accounts/profile'
+        
+        #def form_valid(self, form):
+            #context={}
+            #if all((user_form.is_valid(), profile_form.is_valid())):
+            ##if form.is_valid():
+                #print('forms are valid')
+                #print('cleaned_data: before ->', form.cleaned_data)
+            #else:
+                #print('form not valid', form.errors)
+                #context['errors'] = form.errors
+            #return super().form_valid(form)
+        
+        #def get_object(self):
+            #me = self.request.user
+            ##print('args, kwargs:',self.args, self.kwargs)
+            #id_ = me.id
+            #return get_object_or_404(User, id=id_)
+        
+        #def get_context_data(self, *args, **kwargs):
+            #id_ = self.request.user.id
+            #u = get_object_or_404(User, id=id_)
+            #context = super(UserProfileView, self).get_context_data(*args, **kwargs)
+            #context['groups'] = u.groups.values_list('name',flat=True)
+            #return context
+            
+        
+        ##return render(request, 'accounts/profile.html', context=context)
+    
+    
+
