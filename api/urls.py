eb4b68cd53ba44180fce64362c42c2b6056668bb
Karl Grossner <karl.geog@gmail.com>
2020-04-14 17:16:02 -0400

    refactored api/[datasets,places,geoms,users,areas]

--- api/urls.py
+++ api/urls.py
@@ -33,12 +33,13 @@ urlpatterns = [
     path('datasets/', views.DatasetList.as_view(),name='dataset-list'),    
     path('datasets/<int:pk>/', views.DatasetDetail.as_view(),name='dataset-detail'),
 
-    #path('places/<str:ds>/', views.PlaceList.as_view(),name='place-list'),    
-    #path('places/', views.PlaceList.as_view(),name='place-list'),    
     path('places/', views.PlaceViewSet.as_view({'get': 'list'}),name='place-list'),    
     path('places/<int:pk>/', views.PlaceDetail.as_view(),name='place-detail'),    
 
     path('geoms/', views.GeomViewSet.as_view({'get': 'list'}),name='geom-list'),    
+
+    path('areas/<int:pk>/', views.AreaViewSet.as_view({'get': 'retrieve'}),name='area-detail'),    
+    #path('areas/<int:pk>/', views.AreaViewSet.as_view(),name='area-detail'),    
     
     path('users/', views.UserList.as_view(),name='user-list'),
     path('users/<int:pk>/', views.UserDetail.as_view(),name='user-detail'),    
