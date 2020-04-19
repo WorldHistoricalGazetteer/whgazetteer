9db2e891af3619140e182e9b9bfb143dccb01a8e
Karl Grossner <karl.geog@gmail.com>
2020-04-19 13:06:14 -0400

    pagination & title query for places by dataset

--- api/urls.py
+++ api/urls.py
@@ -2,51 +2,56 @@
 
 from django.conf.urls import url, include
 from django.urls import path
-from rest_framework import routers
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
-# TODO: refactor as for datasets, users
-# ne way or the other
-#router = routers.DefaultRouter()
 
-#router.register(r'places', views.PlaceViewSet)
-#router.register(r'geoms', views.GeomViewSet)
-#router.register(r'areas', views.AreaViewSet)
-#router.register(r'datasets', views.DatasetViewSet)
-#router.register(r'users', views.UserViewSet)
-#router.register(r'groups', views.GroupViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    #url(r'^', include(router.urls)),
-    #url('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-
-    # single union record, ?idx=whg02&_id={whg_id}
-    # TODO: build from place_id
-    url('union/', views.indexAPIView.as_view(), name='union_api'),
-
     path('api-auth/', include('rest_framework.urls')),
     path('', views.api_root),
 
-    path('datasets/', views.DatasetAPIView.as_view(), name='dataset-list'),    
+    # 
+    # *** DATASETS ***
+    # 
+    # public datasets
+    path('datasets/', views.DatasetAPIView.as_view(), name='dataset-list'),
+    # single dataset record
     path('dataset/<int:pk>/', views.DatasetDetailAPIView.as_view(),name='dataset-detail'),
 
-    # drf table browsing dataset places
-    path('placetable/', views.PlaceTableViewSet.as_view({'get':'list'}),name='place-table'), 
+    # 
+    # *** PLACES ***
+    # 
     # places in a dataset
-    path('places/<int:pk>/', views.PlaceList.as_view(),name='place-list'), 
-    # a place
-    path('place/<int:pk>/', views.PlaceDetail.as_view(),name='place-detail'),    
+    # TODO: repurpose for full download
+    path('places/<str:dslabel>/', views.PlaceAPIView.as_view(),name='place-list'), 
 
+    # a place
+    path('place/<int:pk>/', views.PlaceDetailAPIView.as_view(),name='place-detail'),    
+
+    # drf table in dataset.html#browse
+    path('placetable/', views.PlaceTableViewSet.as_view({'get':'list'}),name='place-table'), 
+
+    # geometry for map in dataset.html#browse
     path('geoms/', views.GeomViewSet.as_view({'get':'list'}),name='geom-list'),    
 
+    # 
+    # *** AREAS ***
+    # 
     path('areas/<int:pk>/', views.AreaViewSet.as_view({'get': 'retrieve'}),name='area-detail'),    
-    #path('areas/<int:pk>/', views.AreaViewSet.as_view(),name='area-detail'),    
     
+    # 
+    # *** USERS ***
+    #   
     path('users/', views.UserList.as_view(),name='user-list'),
-    path('users/<int:pk>/', views.UserDetail.as_view(),name='user-detail'),    
+    path('users/<int:pk>/', views.UserDetail.as_view(),name='user-detail'),
+    
+    # 
+    # *** INDEX ***
+    # 
+    # single union record, ?idx=whg02&_id={whg_id}
+    # TODO: build from place_id
+    url('union/', views.indexAPIView.as_view(), name='union_api')
+    
 ]
 
 #urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -10,6 +10,7 @@ from rest_framework import generics
 from rest_framework import permissions
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication
 from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
@@ -25,9 +26,9 @@ from datasets.models import Dataset
 from places.models import Place, PlaceGeom
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 20
+    page_size = 10
     page_size_query_param = 'page_size'
-    max_page_size = 1000
+    #max_page_size = 1000
     
 @api_view(['GET'])
 def api_root(request, format=None):
@@ -37,38 +38,32 @@ def api_root(request, format=None):
         #'datasets': reverse('dataset-list', request=request, format=format)
     })
 
-class PlaceDetail(APIView):
-    """
-    Retrieve a single place database record.
-    """
-    def get_object(self, pk):
-        print('PlaceDetail.get_object()',pk)
-        try:
-            return Place.objects.get(pk=pk)
-        except Place.DoesNotExist:
-            raise Http404
 
-    def get(self, request, pk, format=None):
-        place = self.get_object(pk)
-        serializer = PlaceSerializer(place,context={'request': request})
-        return Response(serializer.data)
-    
-    def foo(self, pk):
-        return JsonResponse({"id":pk})
+#
+# API
+class PlaceDetailAPIView(generics.RetrieveAPIView):
+    """  single place record by id  (database)  """
+    queryset = Place.objects.all()
+    serializer_class = PlaceSerializer
     
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
+    authentication_classes = [SessionAuthentication]
 
-class PlaceList(APIView):
+class PlaceAPIView(generics.ListAPIView):
     """
-    List all places in dataset
+    Paged list of all places in dataset
     """
+    serializer_class = PlaceSerializer
     pagination_class = StandardResultsSetPagination
     
-    def get(self, request, *args, **kwargs):
-        ds= kwargs['pk']
-        places=get_object_or_404(Dataset,pk=ds).places.all()
-        serializer = PlaceSerializer(places, many=True,context={'request': request})
-        return Response(serializer.data) 
+    def get_queryset(self, *args, **kwargs):
+        print('kwargs',self.kwargs)
+        dslabel=self.kwargs['dslabel']
+        qs = Place.objects.all().filter(dataset=dslabel).order_by('title')
+        query = self.request.GET.get('q')
+        if query is not None:
+            qs = qs.filter(title__icontains=query)
+        return qs
     
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
 
@@ -77,6 +72,7 @@ class PlaceList(APIView):
 class DatasetAPIView(generics.ListAPIView):
     """    List public datasets    """
     serializer_class = DatasetSerializer
+    pagination_class = StandardResultsSetPagination
     
     def get_queryset(self):
         qs = Dataset.objects.all().filter(public=True).order_by('label')
@@ -86,34 +82,16 @@ class DatasetAPIView(generics.ListAPIView):
         return qs
     
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
-    authentication_classes = []
+    authentication_classes = [SessionAuthentication]
 
 class DatasetDetailAPIView(generics.RetrieveAPIView):
-    """    Retrieve dataset record by id   """
-
-    queryset = Dataset.objects.all().filter(public=True).order_by('label')
+    """    dataset record by id   """
+    queryset = Dataset.objects.all().filter(public=True)
     serializer_class = DatasetSerializer
     
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
-    authentication_classes = []
+    authentication_classes = [SessionAuthentication]
     
-class DatasetDetail(APIView):
-    """
-    Retrieve a single dataset record.
-    """
-    def get_object(self, pk):
-        try:
-            return Dataset.objects.get(pk=pk)
-        except Dataset.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        dataset = self.get_object(pk)
-        serializer = DatasetSerializer(dataset)
-        return Response(serializer.data)
-    
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
-
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
@@ -124,50 +102,7 @@ class UserDetail(generics.RetrieveAPIView):
     serializer_class = UserSerializer
 
 #
-# in use pre-Apr 2020
-class DatasetViewSet(viewsets.ModelViewSet):
-    # print('in DatasetViewSet()')
-    queryset = Dataset.objects.all().filter(public=True).order_by('label')
-    serializer_class = DatasetSerializer
-
-    def get_permissions(self):
-        """
-        Instantiates and returns the list of permissions that this view requires.
-        """
-        if self.action in ['list','retrieve']:
-            print(self.action)
-            permission_classes = [permissions.AllowAny]
-        else:
-            permission_classes = [permissions.IsAdminUser]
-        return [permission() for permission in permission_classes]
-
-    
-class indexAPIView(View):
-    @staticmethod
-    def get(request):
-        print('in indexAPIView, GET =',request.GET)
-        """
-        args in request.GET:
-        [string] idx: latest name for whg index
-        [string] _id: same as whg_id in index
-        """
-        es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-        idx = request.GET.get('idx')
-        _id = request.GET.get('_id')
-        q={"query": {"bool": {"must": [{"match":{"_id": _id }}]}}}
-        res = es.search(index=idx, doc_type='place', body=q)
-        # single hit (it's a unique id after all)
-        hit = res['hits']['hits'][0]
-        print('hit[_id] from indexAPIView()',hit['_id'])
-        # now get traces
-        # does hit have children?
-        
-        #qt={"query": {"bool": {"must": [{"match":{"_id": _id }}]}}}
-        #res_t = es.search(index="traces", doc_type='trace', body=q)
-        #print('indexAPIView _id',_id)
-        print('indexAPIView hit',hit)
-        return JsonResponse(hit, safe=True)
-        
+# in use pre-Apr 2020    
 class GeomViewSet(viewsets.ModelViewSet):
     queryset = PlaceGeom.objects.all()
     serializer_class = PlaceGeomSerializer
@@ -179,6 +114,7 @@ class GeomViewSet(viewsets.ModelViewSet):
         qs = PlaceGeom.objects.filter(place_id_id__in=dsPlaceIds)
         return qs
     
+#
 # populates drf table in dataset.detail#browse
 class PlaceTableViewSet(viewsets.ModelViewSet):
     queryset = Place.objects.all().order_by('title')
@@ -210,49 +146,64 @@ class PlaceTableViewSet(viewsets.ModelViewSet):
 class AreaViewSet(viewsets.ModelViewSet):
     queryset = Area.objects.all().order_by('title')
     serializer_class = AreaSerializer
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-
-    def get_permissions(self):
+#
+# search index
+class indexAPIView(View):
+    @staticmethod
+    def get(request):
+        print('in indexAPIView, GET =',request.GET)
         """
-        Instantiates and returns the list of permissions that this view requires.
+        args in request.GET:
+        [string] idx: latest name for whg index
+        [string] _id: same as whg_id in index
         """
-        if self.action in ['list','retrieve']:
-            print(self.action)
-            permission_classes = [permissions.AllowAny]
-        else:
-            permission_classes = [permissions.IsAdminUser]
-        return [permission() for permission in permission_classes]
+        es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+        idx = request.GET.get('idx')
+        _id = request.GET.get('_id')
+        q={"query": {"bool": {"must": [{"match":{"_id": _id }}]}}}
+        res = es.search(index=idx, doc_type='place', body=q)
+        # single hit (it's a unique id after all)
+        hit = res['hits']['hits'][0]
+        print('hit[_id] from indexAPIView()',hit['_id'])
+        # now get traces
+        # does hit have children?
+        
+        #qt={"query": {"bool": {"must": [{"match":{"_id": _id }}]}}}
+        #res_t = es.search(index="traces", doc_type='trace', body=q)
+        #print('indexAPIView _id',_id)
+        print('indexAPIView hit',hit)
+        return JsonResponse(hit, safe=True)
+        
+# DREK
+#class UserViewSet(viewsets.ModelViewSet):
+    #queryset = User.objects.all().order_by('-date_joined')
+    #serializer_class = UserSerializer
+
+    #def get_permissions(self):
+        #"""
+        #Instantiates and returns the list of permissions that this view requires.
+        #"""
+        #if self.action in ['list','retrieve']:
+            #print(self.action)
+            #permission_classes = [permissions.AllowAny]
+        #else:
+            #permission_classes = [permissions.IsAdminUser]
+        #return [permission() for permission in permission_classes]
 
 
-class GroupViewSet(viewsets.ModelViewSet):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
 
-#@api_view(['GET'])
-#def dataset_list(request, format=None):
-    #"""
-    #List all datasets
-    #"""
-    #if request.method == 'GET':
-        #datasets = Dataset.objects.all()
-        #serializer = DatasetSerializer(datasets, many=True)
-        #return JsonResponse(serializer.data, safe=False)
-    
+#class DatasetViewSet(viewsets.ModelViewSet):
+    ## print('in DatasetViewSet()')
+    #queryset = Dataset.objects.all().filter(public=True).order_by('label')
+    #serializer_class = DatasetSerializer
 
-#@api_view(['GET'])
-#def dataset_detail(request, pk, format=None):
-    #"""
-    #Retrieve, update or delete a code snippet.
-    #"""
-    #try:
-        #dataset = Dataset.objects.get(pk=pk)
-    #except Dataset.DoesNotExist:
-        #return Response(status=status.HTTP_404_NOT_FOUND)
-
-    #if request.method == 'GET':
-        #serializer = DatasetSerializer(dataset)
-        #return JsonResponse(serializer.data)
-
+    #def get_permissions(self):
+        #"""
+        #Instantiates and returns the list of permissions that this view requires.
+        #"""
+        #if self.action in ['list','retrieve']:
+            #print(self.action)
+            #permission_classes = [permissions.AllowAny]
+        #else:
+            #permission_classes = [permissions.IsAdminUser]
+        #return [permission() for permission in permission_classes]
