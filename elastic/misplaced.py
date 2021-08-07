edfb1c26a842417fe2a0ea27c18c69a476fa7c65
Karl Grossner <karlg@kgeographer.org>
2021-08-07 12:19:11 -0600

    misplaced() start

---
+++ elastic/misplaced.py
@@ -0,0 +1,26 @@
+from elasticsearch import Elasticsearch      
+es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+
+from elastic.es_utils import esq_pid, esq_parent
+
+# 81228 (Alexandria, VA)
+# walk through rearranging docs
+def misplaced(pid, idx):
+  pid = 81228; idx='whg_bak'
+  doc = es.search(index=idx, body=esq_pid(pid))
+  if len(doc['hits']['hits']) > 0
+  print(doc)
+  
+  
+  
+
+
+misplaced(81228, 'whg')
+
+
+# parent and its children, by _id/whg_id (should be same)
+GET /whg/_search
+{"query":{"bool":{"should": [
+    {"parent_id": {"type": "child","id":"14159046"}},
+    {"match":{"_id":"14159046"}}
+]}}}
