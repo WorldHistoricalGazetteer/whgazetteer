cc47ab22dada9ae37402937555a53fc7b2065973
Karl Grossner <karl@kgeographer.org>
2021-12-18 12:28:45 +0100

    descriptions in review.html

--- datasets/views.py
+++ datasets/views.py
@@ -1899,7 +1899,8 @@ def ds_insert_tsv(request, pk):
         
         description = r[header.index('description')] \
           if 'description' in header else ''
-        
+        print('description (src_id)', description)
+
         # create new Place object
         # TODO: generate fclasses
         newpl = Place(
