3f7cde53a45232969ac6bc179621ba8a941dbd3d
Karl Grossner <karl@kgeographer.org>
2022-03-07 16:13:53 -0700

    restored teaching resources, admin screens tweaked

--- resources/admin.py
+++ resources/admin.py
@@ -1,27 +1,43 @@
 from django.contrib import admin
+from guardian.admin import GuardedModelAdmin
 
-# Register your models here.
+# class DatasetAdmin(GuardedModelAdmin):
+#     list_display = ('id', 'label', 'title', 'create_date', 'datatype', 'ds_status')
+# admin.site.register(Dataset,DatasetAdmin)
+#
+# #class DatasetFileAdmin(admin.ModelAdmin):
+# class DatasetFileAdmin(GuardedModelAdmin):
+#     list_display = ('dataset_id_id', 'file', 'upload_date', 'df_status', 'format', 'datatype')
+# admin.site.register(DatasetFile,DatasetFileAdmin)
+
 from .models import Resource, ResourceFile, ResourceImage
+# Register your models here.
 
-class ResourceFileAdmin(admin.StackedInline):
-    model = ResourceFile
+# class ResourceFileAdmin(admin.StackedInline):
+#     list_display = ('','')
+#     class Meta:
+#         model = ResourceFile
+#
+# class ResourceImageAdmin(admin.StackedInline):
+#     list_display = ('','')
+#     class Meta:
+#         model = ResourceImage
 
-class ResourceImageAdmin(admin.StackedInline):
-    model = ResourceImage
+class ResourceAdmin(GuardedModelAdmin):
+    list_display = ('id','create_date','pub_date', 'type', 'title')
+    # inlines = [ResourceFileAdmin, ResourceImageAdmin]
+    # model = Resource
+admin.site.register(Resource, ResourceAdmin)
 
+# @admin.register(ResourceFile)
+class ResourceFileAdmin(GuardedModelAdmin):
+    list_display = ('resource','file')
+    # model = ResourceImage
+admin.site.register(ResourceFile, ResourceFileAdmin)
 
-@admin.register(Resource)
-class ResourceAdmin(admin.ModelAdmin):
-    inlines = [ResourceFileAdmin, ResourceImageAdmin]
-
-    class Meta:
-       model = Resource
-
-
-@admin.register(ResourceFile)
-class ResourceFileAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(ResourceImage)
+# @admin.register(GuardedModelAdmin)
 class ResourceImageAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('resource','image')
+    # model = ResourceImage
+admin.site.register(ResourceImage, ResourceImageAdmin)
+
