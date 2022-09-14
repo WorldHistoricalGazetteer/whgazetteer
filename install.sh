20be82396cc370c03cd1d6be06baf61a38b11c66
Tomer Sagi <ts.tomersagi@gmail.com>
2022-09-14 12:25:11 +0000

    minimal requirements file

--- install.sh
+++ install.sh
@@ -3,3 +3,8 @@
 # run this using sudo
 apt install -y git
 apt install -y python-is-python3
+apt update
+apt upgrade
+apt install -y gdal-bin libgdal-dev gfortran gunicorn
+apt install -y python3-pip
+pip install -r requirements397.txt
