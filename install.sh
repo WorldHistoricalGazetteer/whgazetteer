c66076c4fdc064cd0f675f8e21cecfa342bd56ab
Tomer Sagi <ts.tomersagi@gmail.com>
2022-09-28 06:54:27 +0000

    added db init script

--- install.sh
+++ install.sh
@@ -11,3 +11,5 @@ pip install -r requirements397.txt
 wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
 chmod +x cloud_sql_proxy
 apt install -y htop
+export PGPASSWORD=<password>
+psql -h localhost -d mehdie -U mehdie -p 5432 -a -w -f postgres_init.sql
