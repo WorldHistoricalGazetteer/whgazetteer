e0c9136b5f36f974cec4ec475ca5132e07d732cf
Karl Grossner <karl.geog@gmail.com>
2023-03-28 20:26:45 +0200

    JSONField switch

--- traces/models.py
+++ traces/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.postgres.fields import JSONField, ArrayField
+from django.contrib.postgres.fields import ArrayField
+from django.db.models import JSONField
 from django.core.validators import URLValidator
 from main.choices import ERAS, TRACETYPES, TRACERELATIONS
 
