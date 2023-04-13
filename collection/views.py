eaeeb4edb13742976cd2cff0989497efb9675f54
Karl Grossner <karl.geog@gmail.com>
2023-04-13 16:17:37 +0200

    restore mapbox for collections

--- collection/views.py
+++ collection/views.py
@@ -566,7 +566,6 @@ class DatasetCollectionBrowseView(DetailView):
 
   def get_context_data(self, *args, **kwargs):
     context = super(DatasetCollectionBrowseView, self).get_context_data(*args, **kwargs)
-    context['mbtokenkg'] = settings.MAPBOX_TOKEN_KG
     context['mbtoken'] = settings.MAPBOX_TOKEN_WHG
     context['media_url'] = settings.MEDIA_URL
 
